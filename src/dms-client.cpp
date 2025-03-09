a7dd2a487a5581368e5908ce9a34771092d906ba
Gabriel Szopinski <107429170+Gszopinski123@users.noreply.github.com>
2025-03-09 01:26:27 -0500

    Update dms-client.cpp

--- src/dms-client.cpp
+++ src/dms-client.cpp
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -17,7 +15,7 @@ int main(int argc, char** argv) {
         printf("%s\n",ip);
     }
     struct sockaddr_in address;
-    //ipbasic == -1 ? inet_aton(ip, (struct in_addr*)&address.sin_addr.s_addr)  : address.sin_addr.s_addr = ipbasic;
+    ipbasic == -1 ? inet_aton(ip, (struct in_addr*)&address.sin_addr.s_addr)  : address.sin_addr.s_addr = ipbasic;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
     address.sin_family = AF_INET;
