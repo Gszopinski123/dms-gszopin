97b413ecf87ca8d8f5f4c028ef944fe50bef5f28
Gabriel Szopinski <107429170+Gszopinski123@users.noreply.github.com>
2025-03-09 01:49:33 -0500

    Almost ended it here on this change

--- src/dms-client.cpp
+++ src/dms-client.cpp
@@ -16,7 +16,6 @@ int main(int argc, char** argv) {
     }
     struct sockaddr_in address;
     ipbasic == -1 ? inet_aton(ip, (struct in_addr*)&address.sin_addr.s_addr)  : address.sin_addr.s_addr = ipbasic;
-    address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
     address.sin_family = AF_INET;
     int socketfd = socket(AF_INET,SOCK_STREAM,0);
